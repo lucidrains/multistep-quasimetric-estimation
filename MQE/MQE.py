355edbad0ed7f436ee617b6427ce6c6514e03b31
lucidrains <lucidrains@gmail.com>
2026-07-09 08:36:27 -0700

    scaffold

---
+++ MQE/MQE.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+import torch
+from torch import nn
+from torch.nn import Module, ModuleList
+
+# helpers
+
+def exists(v):
+    return v is not None
+
+def default(v, d):
+    return v if exists(v) else d
+
+# classes
+
+class MQE(Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, state):
+        return state
---
+++ MQE/__init__.py
@@ -0,0 +1,2 @@
+
+from MQE.MQE import MQE
